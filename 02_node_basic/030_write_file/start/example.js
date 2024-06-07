9ccee26746615c182598b76528e35aca76efcff5
Cyanoptilas <75958367+Cyanoptilas@users.noreply.github.com>
2024-06-07 15:15:33 +0900

    add

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,13 @@
-console.log('hello, node.js');
+// 書き込み
+// __dirname
+// __filename
+
+const fs = require("fs");
+
+__dirname;
+console.log("🚀 ~ __dirname:", __filename);
+
+fs.writeFileSync(__dirname + "/test.txt", "hello nodejs!!");
+// ↑ nodeコマンドを実行したときのカレントディレクトリからの相対パス
+// このファイルからの相対パスではない
+console.log("hello, node.js");
